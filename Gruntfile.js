6a58bd39812d2e7efac93da98503f2c66eb490f9
Lemoncog <jason@localhost.(none)>
2015-01-10 19:56:09 +0000

    Tweaks to layout of text, added WIP date label

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
     watch: {
       options: {
         nospawn: true,
-        livereload: { liveCSS: false }
+        livereload: { liveCSS: true }
       },
       livereload: {
         options: {
